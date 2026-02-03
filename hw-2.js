4198e9169741cece92313041c115b12314cdcc75
osilver17 <osilver@internet.ru>
2026-02-03 17:45:41 +0300

    Made the JS hw-3

--- hw-2.js
+++ hw-2.js
@@ -1,65 +1,65 @@
-// Задание 1
+// // Задание 1
 
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
 
-// Задание 2
+// // Задание 2
 
-const b = 2007;
-alert(b);
+// const b = 2007;
+// alert(b);
 
-// Задание 3
+// // Задание 3
 
-const c = "Brendan Eich";
-alert(c);
+// const c = "Brendan Eich";
+// alert(c);
 
-// Задание 4
+// // Задание 4
 
-let d = 10;
-let e = 2;
-alert(d + e);
-alert(d - e);
-alert(d * e);
-alert(d / e);
+// let d = 10;
+// let e = 2;
+// alert(d + e);
+// alert(d - e);
+// alert(d * e);
+// alert(d / e);
 
-// Задание 5
+// // Задание 5
 
-let result = Math.pow(2, 5);
-alert(result);
+// let result = Math.pow(2, 5);
+// alert(result);
 
-// Задание 6
+// // Задание 6
 
-let aa = 9;
-let bb = 2;
-alert(aa % bb);
+// let aa = 9;
+// let bb = 2;
+// alert(aa % bb);
 
-// Задание 7
+// // Задание 7
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
 
-// Задание 8
+// // Задание 8
 
-let age = prompt("Сколько Вам лет?");
-alert(age);
+// let age = prompt("Сколько Вам лет?");
+// alert(age);
 
-// Задание 9
+// // Задание 9
 
-const user = {
-    name: "Вася",
-    age: 25,
-    isAdmin: true,
-}
+// const user = {
+//     name: "Вася",
+//     age: 25,
+//     isAdmin: true,
+// }
 
-// Задание 10
+// // Задание 10
 
-let userName = prompt("Введите Ваше имя?", user.name);
-alert(`Привет, ${userName}!`);
+// let userName = prompt("Введите Ваше имя?", user.name);
+// alert(`Привет, ${userName}!`);
