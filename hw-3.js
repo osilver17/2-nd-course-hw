dc52ba6c8dc44ce6b3d2dc5e2b6a0e415119c0a2
osilver17 <osilver@internet.ru>
2026-02-05 11:55:36 +0300

    Renamed hw-4 file name

--- hw-3.js
+++ hw-3.js
@@ -1,107 +1,107 @@
-// Задание 1
-console.log("Задание 1");
+// // Задание 1
+// console.log("Задание 1");
 
-let password = 'qwerty';
+// let password = 'qwerty';
 
-let typingPassWord = prompt("Введите пароль.");
-console.log("typingPassWord =", typingPassWord);
-console.log("Верный пароль =", password);
+// let typingPassWord = prompt("Введите пароль.");
+// console.log("typingPassWord =", typingPassWord);
+// console.log("Верный пароль =", password);
 
-if (typingPassWord === password) {
-    console.log("Пароль введен верно.");
-    alert("Пароль введен верно.");
-}
-else {
-    console.log("Пароль введен неправильно.");
-    alert("Пароль введен неправильно.");
-}
-console.log("");
+// if (typingPassWord === password) {
+//     console.log("Пароль введен верно.");
+//     alert("Пароль введен верно.");
+// }
+// else {
+//     console.log("Пароль введен неправильно.");
+//     alert("Пароль введен неправильно.");
+// }
+// console.log("");
 
 
-// Задание 2
-console.log("Задание 2");
+// // Задание 2
+// console.log("Задание 2");
 
-let c = 5;
-console.log("c =", c);
-console.log("c > 0 && c < 10 ?");
-c > 0 && c < 10 ? console.log("Верно.") : console.log("Неверно.");
-console.log("");
+// let c = 5;
+// console.log("c =", c);
+// console.log("c > 0 && c < 10 ?");
+// c > 0 && c < 10 ? console.log("Верно.") : console.log("Неверно.");
+// console.log("");
 
-const arrOfC = [0, 10, -3, 2];
+// const arrOfC = [0, 10, -3, 2];
 
-for (let i = 0; i < 4; i++) {
-    let newC = arrOfC[i];
-    console.log("c =", newC);
-    console.log("c > 0 && c < 10 ?");
-    newC > 0 && newC < 10 ? console.log("Верно.") : console.log("Неверно.");
-    console.log("");
-}
+// for (let i = 0; i < 4; i++) {
+//     let newC = arrOfC[i];
+//     console.log("c =", newC);
+//     console.log("c > 0 && c < 10 ?");
+//     newC > 0 && newC < 10 ? console.log("Верно.") : console.log("Неверно.");
+//     console.log("");
+// }
 
-// Задание 3
-console.log("Задание 3");
+// // Задание 3
+// console.log("Задание 3");
 
-let d = 500;
-let e = 5;
+// let d = 500;
+// let e = 5;
 
-console.log("d =", d, "e =", e);
-console.log("d > 100 || e > 100 ?");
-d > 100 || e > 100 ? console.log("Верно.") : console.log("Неверно.");
-console.log("");
+// console.log("d =", d, "e =", e);
+// console.log("d > 100 || e > 100 ?");
+// d > 100 || e > 100 ? console.log("Верно.") : console.log("Неверно.");
+// console.log("");
 
-// Задание 4
-console.log("Задание 4");
+// // Задание 4
+// console.log("Задание 4");
 
-let a = '2';
-let b = '3';
-// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+// let a = '2';
+// let b = '3';
+// // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
 
-let result = Number(a) + Number(b);
+// let result = Number(a) + Number(b);
 
-console.log("Сумма a + b =", result);
+// console.log("Сумма a + b =", result);
 
-alert(`Сумма a + b = ${result}`);
+// alert(`Сумма a + b = ${result}`);
 
-console.log("");
+// console.log("");
 
-// Задание 5
-console.log("Задание 5");
+// // Задание 5
+// console.log("Задание 5");
 
-let monthNumber = Number(prompt("Введите номер месяца: (январь - это месяц №1)"));
+// let monthNumber = Number(prompt("Введите номер месяца: (январь - это месяц №1)"));
 
-switch (monthNumber) {
-    case 12:
-    case 1:
-    case 2:
-        console.log("monthNumber =", monthNumber);
-        console.log("Это зима!");
-        alert("Это зима!");
-        break;
-    case 3:
-    case 4:
-    case 5:
-        console.log("monthNumber =", monthNumber);
-        console.log("Это весна!");
-        alert("Это весна!");
-        break;
-    case 6:
-    case 7:
-    case 8:
-        console.log("monthNumber =", monthNumber);
-        console.log("Это лето!");
-        alert("Это лето!");
-        break;
-    case 9:
-    case 10:
-    case 11:
-        console.log("monthNumber =", monthNumber);
-        console.log("Это осень!");
-        alert("Это осень!");
-        break;
+// switch (monthNumber) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log("monthNumber =", monthNumber);
+//         console.log("Это зима!");
+//         alert("Это зима!");
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("monthNumber =", monthNumber);
+//         console.log("Это весна!");
+//         alert("Это весна!");
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log("monthNumber =", monthNumber);
+//         console.log("Это лето!");
+//         alert("Это лето!");
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log("monthNumber =", monthNumber);
+//         console.log("Это осень!");
+//         alert("Это осень!");
+//         break;
 
-    default:
-        console.log("monthNumber =", monthNumber);
-        console.log("Вы не ввели номер месяца! Нужен был номер месяца!");
-        alert("Вы не ввели номер месяца! Нужен был номер месяца!");
-        break;
-}
+//     default:
+//         console.log("monthNumber =", monthNumber);
+//         console.log("Вы не ввели номер месяца! Нужен был номер месяца!");
+//         alert("Вы не ввели номер месяца! Нужен был номер месяца!");
+//         break;
+// }
 
